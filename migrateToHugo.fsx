b70c9fe0c864b8d4473324c8c68db06c0dd1b864
Mark Allibone <mallibone@outlook.com>
2019-08-05 07:52:00 +0200

    Start parsing content

--- migrateToHugo.fsx
+++ migrateToHugo.fsx
@@ -5,9 +5,28 @@ open FSharp.Data
 open System
 open System.IO
 
-let filename = "C:/Work/blog/migrateToHugo/posts/06f314df-5ea2-41e2-9024-193077376e41.xml"
+type Block =
+    | RawText of string
+    | ItalicText of string
+    | BoldText of string
+    | Link of string * string // txt, url
+    | Image of string * string // alt, url
+
+type Language =
+    | CSharp
+    | FSharp
+    | VB
+    | Other
+
+type Content =
+    | Paragraph of Block list
+    | Header of string
+    | Code of Language * string
+    | Problem of string
 
 type BlogPost = XmlProvider<"C:/Work/blog/migrateToHugo/posts/06f314df-5ea2-41e2-9024-193077376e41.xml">
+
+let filename = "C:/Work/blog/migrateToHugo/posts/06f314df-5ea2-41e2-9024-193077376e41.xml"
 let blog = BlogPost.Load(filename)
 
 let formatTags (tags:string array) =
@@ -26,6 +45,35 @@ let createHeader (blog:BlogPost.Post) =
         "---" ]
     |> String.concat "\n"
 
+
+let paragraphTokens = ("<p>","</p>")
+let codeTokens = ("<pre ","</pre>")
+let headerTokens = ("<h2>","</h2>")
+
+let (|BlockBetween|_|) (openToken:string,closeToken:string) (content:string) =
+    if content.Length = 0 then None
+    elif content.StartsWith openToken then
+        let endIndex = content.IndexOf closeToken
+        let block = content.Substring(0, endIndex + closeToken.Length)
+        let rest = content.Substring(endIndex + closeToken.Length)
+        Some(block,rest)
+    else None
+
+let rec pageComponents acc (txt:string) =
+    match txt with
+    | BlockBetween paragraphTokens (block,rest) -> 
+        let blocks = parseParagraph block
+        pageComponents ((Paragraph blocks) :: acc) rest
+    | BlockBetween codeTokens (block,rest) -> 
+        let code = parseCode block
+        pageComponents (code :: acc) rest
+    | BlockBetween headerTokens (block,rest) -> 
+        let header = parseHeader block
+        pageComponents (header :: acc) rest
+    | Malformed ["<p>";"<pre ";"<h2>"] (block,rest) -> 
+        pageComponents ((Problem block) :: acc) rest
+    | _ -> acc |> List.rev 
+
 let header = createHeader blog
 let content = "TODO"
 
@@ -36,4 +84,6 @@ let outputDirectory = Path.Combine(__SOURCE_DIRECTORY__, "_posts")
 Directory.CreateDirectory(outputDirectory)
 
 File.WriteAllText(Path.Combine(outputDirectory, postname), post)
+
+blog.Content
 
